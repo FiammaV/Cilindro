9fcd86f19e2e8a6a598c93d46b8c2c3b623b05f7
Inês Nunes <ines.perdigao.nunes@gmail.com>
2018-02-22 12:18:59 +0000

    Volume e area

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace Cilindro {
     class Program {
         static void Main(string[] args) {
+            int a;
+            int r;
+            double V;
+            double area;
+            double PI = 3.1415926;
+            Console.WriteLine("Insira altura");
+            string alt = Console.ReadLine();
+            Console.WriteLine("Insira raio");
+            string raio = Console.ReadLine();
+            a = Convert.ToInt32(alt);
+            r = Convert.ToInt32(raio);
+
+            V = PI * Math.Pow(r, 2) * a;
+            area = 2 * PI * r *(r + a);
+
+            Console.WriteLine("O volume e: " + V);
+            Console.WriteLine("A area e: " + area);
+            Console.ReadKey();
         }
     }
 }
