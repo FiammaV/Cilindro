5b659e2cc2d4fc15122ceb45db8e186ba4a87568
Inês Nunes <ines.perdigao.nunes@gmail.com>
2018-02-22 12:22:24 +0000

    Pequenos ajustes

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,15 +7,17 @@ using System.Threading.Tasks;
 namespace Cilindro {
     class Program {
         static void Main(string[] args) {
+            string raio;
+            string alt;
             int a;
             int r;
             double V;
             double area;
             double PI = 3.1415926;
             Console.WriteLine("Insira altura");
-            string alt = Console.ReadLine();
+            alt = Console.ReadLine();
             Console.WriteLine("Insira raio");
-            string raio = Console.ReadLine();
+            raio = Console.ReadLine();
             a = Convert.ToInt32(alt);
             r = Convert.ToInt32(raio);
 
